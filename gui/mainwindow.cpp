a242d9643d89e6dc980423608843b625bbbb7942
Ahleroy <alix.leroy@net.estia.fr>
2016-02-10 10:40:40 +0100

    Update mainwindow.cpp
    Deleting auto type for cascade_file_menu which is C++11, not always supported by Qt Creator (type changed to Qmenu* in mainWindow.h)

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -5,7 +5,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     face_cascade_ = new QAction("Change Face Cascade", this);
     this->connect(face_cascade_, SIGNAL(triggered()), SLOT(change_face_cascade()));
 
-    auto cascade_file_menu = this->menuBar()->addMenu(tr("File"));
+    cascade_file_menu = this->menuBar()->addMenu(tr("File"));
     cascade_file_menu->addAction(face_cascade_);
 
     DisplayWidget* display = new DisplayWidget(this);
